e7989c424168c3d34043ade1793d02eeac6a7d47
brgxd <121587866+brgxd@users.noreply.github.com>
2023-03-15 15:20:29 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,74 @@
+﻿// MultInherit.cpp: определяет точку входа для консольного приложения.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class A1
+{
+protected:
+	int x;
+public:
+	A1() { x = 0; cout << "\nA1 default constructor " << x << "\n"; }
+	A1(int B) { x = B; cout << "\nA1 constructor " << x << "\n"; }
+	~A1() { cout << "\nA1 destructor"; }
+};
+
+class B1 : virtual public A1
+{
+private:
+	int d;
+public:
+	B1(int D, int B) : A1(B)
+	{
+		d = D; cout << "\nB1 constructor " << d << "\n";
+	}
+	void Show() { cout << "\n" << d << "\t" << x; }
+};
+
+class B2 : virtual public A1
+{
+private:
+	int d;
+public:
+	B2(int D, int B) : A1(B)
+	{
+		d = D; cout << "\nB2 constructor " << d << "\n";
+	}
+	void Show() { cout << "\n" << d << "\t" << x; }
+};
+
+class C1 : virtual public B1, virtual public B2
+{
+private:
+	int d;
+public:
+	C1(int D, int B1, int B2) : B1(D, B1), B2(D, B2)
+	{
+		d = D; cout << "\nC1 constructor " << d << "\n";
+	}
+	void Show() { cout << "\n" << d; }
+
+	int f1()
+	{
+		return B1::x * B1::x;
+	}
+
+	int f2()
+	{
+		return B2::x * B2::x;
+	}
+};
+
+int main()
+{
+	B1 b1(1, 2);
+	B2 b2(3, 4);
+
+	C1 c1(1, 2, 3);
+	c1.Show();
+
+	char c; cin >> c;
+	return 0;
+}
